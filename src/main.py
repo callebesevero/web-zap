02a931c223444623ea6c367cd53317402d2a2fd6
Callebe Severo <callebesev@gmail.com>
2026-01-18 21:39:01 -0300

    Organizing

---
+++ src/main.py
@@ -0,0 +1,4 @@
+# https://docs.streamlit.io/develop/api-reference/user/st.login
+# https://docs.streamlit.io/develop/api-reference/user/st.user
+# https://docs.streamlit.io/develop/api-reference/caching-and-state/st.session_state    
+    # runner.enforceSerializableSessionState
