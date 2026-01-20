dc449aa282754a7776d161d44c048d5d9a58cc6b
Callebe Severo <callebesev@gmail.com>
2026-01-20 18:05:42 -0300

    Works

--- src/main.py
+++ src/main.py
@@ -3,38 +3,49 @@
 # https://docs.streamlit.io/develop/api-reference/caching-and-state/st.session_state    
     # runner.enforceSerializableSessionState
 
-from streamlit import chat_input, chat_message, session_state, set_page_config, title, cache_resource, fragment
+import streamlit as st
 
 
-@cache_resource
+def login():
+    name = st.text_input(label='Type your username:', key='temp_user').capitalize().strip()
+
+    if st.button('Log-in'):
+        st.session_state['user'] = name
+
+
+@st.cache_resource
 def list_init():
     return []
 
 
-@fragment(run_every=0.5)
+@st.fragment(run_every=0.5)
 def show_chat():
     global message_list
     message_list = list_init()
     for mss in message_list:
-        chat_message(avatar=mss['avatar'], name=mss['name'], width='content').write(mss['content'])
+        st.chat_message(avatar=None, name=mss['name'], width='content').write(mss['content'])
 
 
 def main():
-    set_page_config(page_title='Web Zap', page_icon='😁')
-    title('Web Zap 😁')
+    if not 'user' in st.session_state:
+        login()
+    else:
+        st.set_page_config(page_title='Web Zap', page_icon='💬')
+        st.title('Web Zap 💬')
+        st.write(f'Welcome, {st.session_state['user']}!')
 
-    message = chat_input('Type your message:')
+        message = st.chat_input('Type your message:')
 
-    if message:
-        user_message_dict = {
-            'avatar': 'user',
-            'name': 'user',
-            'content': message
-        }
+        if message:
+            user_message_dict = {
+                'name': st.session_state['user'],
+                'content': message
+            }
 
-        show_chat()
+            show_chat()
 
-        message_list.append(user_message_dict)
+            message_list.append(user_message_dict)
+
 
 if __name__=='__main__':
     main()
