6f829386e688f45295428d38e2aa79229ff7fe38
Callebe Severo <callebesev@gmail.com>
2026-01-20 15:15:53 -0300

    Shared chat

--- src/main.py
+++ src/main.py
@@ -5,31 +5,36 @@
 
 from streamlit import chat_input, chat_message, session_state, set_page_config, title, cache_resource, fragment
 
-fragment(run_every=1)
 
-cache_resource.clear()
-@cache_resource # return the back session_state (don't show new messages)
-def cache_messages(message_list):
-    return message_list
+@cache_resource
+def list_init():
+    return []
 
 
-all_mss = cache_messages(session_state['message_list'])
-set_page_config(page_title='Web Zap', page_icon='😁')
-title('Web Zap 😁')
+@fragment(run_every=0.5)
+def show_chat():
+    global message_list
+    message_list = list_init()
+    for mss in message_list:
+        chat_message(avatar=mss['avatar'], name=mss['name'], width='content').write(mss['content'])
 
-if not 'message_list' in session_state:
-    session_state['message_list'] = []
 
-for mss in cache_messages(session_state['message_list']):
-    chat_message(avatar=mss['avatar'], name=mss['name'], width='content').write(mss['content'])
+def main():
+    set_page_config(page_title='Web Zap', page_icon='😁')
+    title('Web Zap 😁')
 
-message = chat_input('Type your message:')
-if message:
-    chat_message(name='user', avatar='user', width='content').write(message)
-    user_message_dict = {
-        'avatar': 'user',
-        'name': 'user',
-        'content': message
-    }
+    message = chat_input('Type your message:')
 
-    session_state['message_list'].append(user_message_dict)
+    if message:
+        user_message_dict = {
+            'avatar': 'user',
+            'name': 'user',
+            'content': message
+        }
+
+        show_chat()
+
+        message_list.append(user_message_dict)
+
+if __name__=='__main__':
+    main()
