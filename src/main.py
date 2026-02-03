880b42c909e92c152e5f4bf474a665e7e1d46b77
Callebe Severo <callebesev@gmail.com>
2026-02-02 22:26:43 -0300

    error fixed

--- src/main.py
+++ src/main.py
@@ -1,28 +1,47 @@
 import streamlit as st
 
 
-def login():
-    name = st.text_input(label='Type your username:', key='temp_user').capitalize().strip()
+def login(
+        signup_code: int = 0
+):
+    name = st.text_input(label='Type your username:', key=f'signup{signup_code}').capitalize().strip()
 
     if st.button('Log-in'):
-        st.session_state['user'] = name
+        if not name in signed_users:
+            signed_users.append(name)
+            st.session_state['user'] = name
+        else:
+            st.error('This name already is signed. Try other!')
+            signup_code += 1
 
 
 @st.cache_resource
-def list_init():
+def get_message_list():
+    return []
+
+
+@st.cache_resource
+def get_signed_users():
     return []
 
 
 @st.fragment(run_every=0.5)
 def show_chat():
     global message_list
-    message_list = list_init()
-    for mss in message_list:
-        st.chat_message(avatar=None, name=mss['name'], width='content').write(mss['content'])
+
+    message_list = get_message_list()
+    try:
+        for mss in message_list:
+            st.chat_message(avatar=None, name=mss['name'], width='content').write(mss['content'])
+    except:
+        return
 
 
 def main():
+    global signed_users
+
     if not 'user' in st.session_state:
+        signed_users = get_signed_users()
         login()
     else:
         st.set_page_config(page_title='Web Zap', page_icon='💬')
@@ -34,7 +53,7 @@ def main():
         if message:
             user_message_dict = {
                 'name': st.session_state['user'],
-                'content': message
+                'content': f'({st.session_state['user']}) {message}'
             }
 
             show_chat()
